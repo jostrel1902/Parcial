20ef596696752bd7a57d21419a1c904fb2ab4934
jostrel1902 <127359138+jostrel1902@users.noreply.github.com>
2023-05-06 21:05:35 -0500

    Delete Parcial-main (2)/Parcial-main directory

--- Parcial-main (2)/Parcial-main/manage_store/src/main/java/co/edu/umanizales/manage_store/controller/dto/ErrorDTO.java
+++
@@ -1,9 +0,0 @@
-package co.edu.umanizales.manage_store.controller.dto;
-
-import lombok.Data;
-
-@Data
-public class ErrorDTO {
-    private int code;
-    private String message;
-}
--- Parcial-main (2)/Parcial-main/manage_store/src/main/java/co/edu/umanizales/manage_store/model/Seller.java
+++
@@ -1,9 +0,0 @@
-package co.edu.umanizales.manage_store.model;
-
-import lombok.Data;
-
-@Data
-public class Seller {
-    private String code;
-    private String name;
-}
